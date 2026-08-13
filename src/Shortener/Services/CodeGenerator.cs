1c09669f512d180f9af4732cd463de522a2deef0
NadiaMlkii <nadia.maleki.dev@gmail.com>
2026-08-13 18:01:45 +0400

    feat: add code generator

---
+++ src/Shortener/Services/CodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Shortener.Services;
+
+public class CodeGenerator
+{
+    public static string Generate(string longUrl)
+    {
+        var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(longUrl));
+        return Convert.ToBase64String(hashBytes)
+            .Replace("+", "-")
+            .Replace("/", "_")
+            .Replace("=", "")
+            [..8];
+    }
+}
